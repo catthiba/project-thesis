bcd3596f4dfad9e92fa0c3debda441e7a8d4cc7a
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2021-12-14 17:06:11 +0100

    Restructure with folders

--- src/xdf.py
+++ src/xdf.py
@@ -3,7 +3,11 @@ import pyxdf
 import matplotlib.pyplot as plt
 import numpy as np
 
-data, header = pyxdf.load_xdf('gsr_test2.xdf')
+data, header = pyxdf.load_xdf('test_GSR_ECG.xdf')
+
+for idx, stream in enumerate(data): 
+    name = stream["info"]["name"][0]
+    print(idx, name)
 
 for stream in data:
     y = np.array(stream['time_series'])
@@ -13,6 +17,7 @@ for stream in data:
 
         # list of strings, draw one vertical line for each marker
         for timestamp, marker in zip(np.array(stream['time_stamps']), y):
+            print(f'Marker "{marker[0]}" @ {timestamp:.2f}s')
             plt.axvline(x=timestamp)
             print(f'Marker "{marker[0]}" @ {timestamp:.2f}s')
     elif isinstance(y, np.ndarray):
